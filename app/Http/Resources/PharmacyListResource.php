d4b3cdc178dd5ec2aac2ae34f6ce6932604cc11e
jammersonf <jammersonf@gmail.com>
2020-06-17 15:29:26 -0300

    pharmacy resource fix

--- app/Http/Resources/PharmacyListResource.php
+++ app/Http/Resources/PharmacyListResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Http\Resources\CityResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
@@ -12,10 +13,9 @@ use Illuminate\Http\Resources\Json\JsonResource;
  *     @OA\Property(property="id", type="integer", example="1"),
  *     @OA\Property(property="code", type="integer", example="01"),
  *     @OA\Property(property="cnpj", type="string", example="99.999.999/0001-91"),
- *     @OA\Property(property="companyName", type="string", example="Teste"),
+ *     @OA\Property(property="socialName", type="string", example="Teste"),
  *     @OA\Property(property="status", type="string", example="ACTIVE"),
- *     @OA\Property(property="cityId", type="integer", example="10"),
- *     @OA\Property(property="cityName", type="string", example="Suzano"),
+ *     @OA\Property(property="city", ref="#/components/schemas/CityResource"),
  *     @OA\Property(property="commercial", type="string", example="Teste 02"),
  *     @OA\Property(property="createdAt",  type="datetime", example="2020-05-25T05:09:15.000000Z"),
  * )
@@ -34,10 +34,9 @@ class PharmacyListResource extends JsonResource
             'id' => $this->id,
             'code' => $this->code,
             'cnpj' => $this->cnpj,
-            'companyName' => $this->company_name,
+            'socialName' => $this->company_name,
             'status' => $this->status,
-            'cityId' => $this->city_id,
-            'cityName' => $this->city->name,
+            'city' => CityResource::make($this->city_id),
             'commercial' => $this->commercial,
             'createdAt' => $this->created_at
         ];
