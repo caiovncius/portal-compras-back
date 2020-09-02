a486294c5b778ccd936d5617ba0e9e9d01bd437b
Caio Vinicius <caio@vncius.com>
2020-09-02 15:00:05 -0300

    fix publicidades

---
+++ database/migrations/2020_09_02_175903_edit_publicity_column_name.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditPublicityColumnName extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('publicities', function (Blueprint $table) {
+            $table->renameColumn('desc', 'description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('publicities', function (Blueprint $table) {
+            //
+        });
+    }
+}
