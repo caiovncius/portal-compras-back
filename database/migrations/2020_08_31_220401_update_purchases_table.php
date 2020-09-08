ed130ed1ad6f85fe50f45fd72c9eab9bb8c7670c
Caio Vinicius <caio@vncius.com>
2020-09-08 06:45:37 -0300

    etste

--- database/migrations/2020_08_31_220401_update_purchases_table.php
+++ database/migrations/2020_08_31_220401_update_purchases_table.php
@@ -14,17 +14,17 @@ class UpdatePurchasesTable extends Migration
     public function up()
     {
         Schema::table('purchases', function (Blueprint $table) {
-            $table->renameColumn('sendType', 'send_type');
-            $table->renameColumn('validityStart', 'validity_start');
-            $table->renameColumn('validityEnd', 'validity_end');
-            $table->renameColumn('untilBilling', 'until_billing');
-            $table->renameColumn('setMinimumBillingValue', 'set_minimum_billing_value');
-            $table->renameColumn('minimumBillingValue', 'minimum_billing_value');
-            $table->renameColumn('setMinimumBillingQuantity', 'set_minimum_billing_quantity');
-            $table->renameColumn('minimumBillingQuantity', 'minimum_billing_quantity');
-            $table->renameColumn('totalIntentionsValue', 'total_intentions_value');
-            $table->renameColumn('totalIntentionsQuantity', 'total_intentions_quantity');
-            $table->renameColumn('relatedQuantity', 'related_quantity');
+//            $table->renameColumn('sendType', 'send_type');
+//            $table->renameColumn('validityStart', 'validity_start');
+//            $table->renameColumn('validityEnd', 'validity_end');
+//            $table->renameColumn('untilBilling', 'until_billing');
+//            $table->renameColumn('setMinimumBillingValue', 'set_minimum_billing_value');
+//            $table->renameColumn('minimumBillingValue', 'minimum_billing_value');
+//            $table->renameColumn('setMinimumBillingQuantity', 'set_minimum_billing_quantity');
+//            $table->renameColumn('minimumBillingQuantity', 'minimum_billing_quantity');
+//            $table->renameColumn('totalIntentionsValue', 'total_intentions_value');
+//            $table->renameColumn('totalIntentionsQuantity', 'total_intentions_quantity');
+//            $table->renameColumn('relatedQuantity', 'related_quantity');
         });
     }
 
