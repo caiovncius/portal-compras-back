65339fb97b7bb3fc8ce2ade142433d39eb77246d
Caio Vinicius <caio@vncius.com>
2020-09-10 15:29:48 -0300

    ajuste

--- app/Http/Requests/OfferUpdatorRequest.php
+++ app/Http/Requests/OfferUpdatorRequest.php
@@ -94,14 +94,14 @@ class OfferUpdatorRequest extends FormRequest
             'products.*.productId' => 'required',
             'products.*.discountDeferred' => 'required|numeric',
             'products.*.discountOnCash' => 'required|numeric',
-            'products.*.minimum' => 'numeric',
+            'products.*.minimum' => 'string|nullable',
             'products.*.minimumPerFamily' => 'required|required',
             'products.*.obrigatory' => 'boolean',
-            'products.*.factoryPrice' => 'numeric',
+            'products.*.factoryPrice' => 'string|nullable',
             'products.*.priceDeferred' => 'required|numeric',
             'products.*.priceOnCash' => 'required|numeric',
-            'products.*.quantityMaximum' => 'numeric',
-            'products.*.quantityMinimum' => 'numeric',
+            'products.*.quantityMaximum' => 'numeric|nullable',
+            'products.*.quantityMinimum' => 'numeric|nullable',
             'products.*.stateId' => 'required|required',
         ];
     }
