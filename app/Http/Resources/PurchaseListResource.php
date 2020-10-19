5909a1b2bc6512887e85b1df58cb7c9d3c916367
Caio Vinicius <caio@vncius.com>
2020-10-18 22:51:23 -0300

    Fix issue #154

--- app/Http/Resources/PurchaseListResource.php
+++ app/Http/Resources/PurchaseListResource.php
@@ -64,7 +64,7 @@ class PurchaseListResource extends JsonResource
 
         return [
             'id' => $this->id,
-            'image' => env('APP_URL') . $this->image,
+            'image' => is_null($this->image) ? null : env('APP_URL') . $this->image,
             'code' => $this->code,
             'name' => $this->name,
             'sendType' => $this->send_type,
