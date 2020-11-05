796bc34e7e70861649e51e3c5e9018f51354aedf
Caio Vinicius <caio@vncius.com>
2020-11-05 09:41:19 -0300

    Corrigido portal de compras

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,18 +20,18 @@ class Cors
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE, PATCH',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, X-Auth-Token, Origin, Authorization'
+            'Access-Control-Allow-Headers'     => 'Content-Type, X-Auth-Token, Origin, Host, Accept-Language, Authorization'
         ];
 
         if ($request->isMethod('OPTIONS')) {
             return response()->json('{"method":"OPTIONS"}', 200, $headers);
         }
 
-        $response = $next($request);
-        foreach ($headers as $key => $value) {
-            $response->headers->set($key, $value);
-        }
-
-        return $response;
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE, PATCH')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Max-Age', '86400')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Host, Accept-Language, Authorization');
     }
 }
